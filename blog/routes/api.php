b8bfc83d929ccffcabb5150beff9b49dab32565c
Shoro749 <romanbro749@gmail.com>
2026-05-24 20:33:38 +0000

    feat: controllers, routes, templates

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\Blog\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'blog'], function () {
+    Route::apiResource('posts', \App\Http\Controllers\Api\Blog\PostController::class)->names('blog.posts');
+});
