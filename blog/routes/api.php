467e87541701a80cd018a74a76b907de0e76db06
Shoro749 <romanbro749@gmail.com>
2026-06-01 18:28:46 +0000

    feet: implement BlogPostRepository and posts admin routes for lab 8

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -16,7 +16,13 @@ Route::group(['prefix' => 'blog'], function () {
 Route::group(['prefix' => 'admin/blog'], function () {
     $methods = ['index', 'store', 'update'];
 
+    //BlogCategory
     Route::apiResource('categories', \App\Http\Controllers\Api\Blog\Admin\CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
+
+    //BlogPost
+    Route::apiResource('posts', \App\Http\Controllers\Api\Blog\Admin\PostController::class)
+        ->except(['show']) //не робити маршрут для метода show
+        ->names('blog.admin.posts');
 });
