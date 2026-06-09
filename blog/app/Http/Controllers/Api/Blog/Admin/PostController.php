7a0b13a8bbcab25ee560422b4ea99d4a0cdfc233
Shoro749 <romanbro749@gmail.com>
2026-06-09 19:42:55 +0000

    feat: add post creation validation, soft delete and category parent accessor

--- blog/app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ blog/app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api\Blog\Admin;
 
+use App\Models\BlogPost;
+use App\Http\Requests\BlogPostCreateRequest;
 use App\Repositories\BlogPostRepository;
 use App\Repositories\BlogCategoryRepository;
 use App\Http\Requests\BlogPostUpdateRequest;
@@ -27,9 +29,24 @@ class PostController extends BaseController
     /**
      * Store a newly created resource in storage.
      */
-    public function store(BlogPostUpdateRequest $request)
+    public function store(BlogPostCreateRequest $request)
     {
-        //
+        $data = $request->input(); //отримаємо масив даних, які надійшли з форми
+
+        $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
+
+        if ($item) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Успішно збережено',
+                'data'    => $item
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Помилка збереження'
+            ], 500);
+        }
     }
 
     /**
@@ -73,6 +90,20 @@ class PostController extends BaseController
      */
     public function destroy(string $id)
     {
-        //
+        $result = BlogPost::destroy($id); //софт деліт, запис лишається
+
+        //$result = BlogPost::find($id)->forceDelete(); //повне видалення з БД
+
+        if ($result) {
+            return response()->json([
+                'success' => true,
+                'message' => "Запис id=[{$id}] успішно видалено"
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Помилка при видаленні запису'
+            ], 500);
+        }
     }
 }
